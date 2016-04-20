21a3dcfb8098af604bdb4cd20fa1879608e25898
Witalij Kaa <witalijkaa@hotmail.com>
2016-04-20 18:43:27 +0300

    wkPHP niceUnixTime

--- javaScript/wkphp.js
+++ javaScript/wkphp.js
@@ -45,5 +45,42 @@ var php = {
     },
     isNumberOrString: function (val) {
         return 'number' == typeof val || 'string' == typeof val;
+    },
+    rusMonths: {
+        1: 'янв',
+        2: 'фев',
+        3: 'мар',
+        4: 'апр',
+        5: 'май',
+        6: 'июн',
+        7: 'июл',
+        8: 'авг',
+        9: 'сен',
+        10: 'окт',
+        11: 'ноя',
+        12: 'дек'
+    },
+
+    niceUnixTime: function (format, timestamp) {
+        var timeDate;
+
+        if ('undefined' == typeof timestamp) {
+            timeDate = new Date();
+        } else {
+            timeDate = new Date(+timestamp * 1000);
+        }
+
+        var minutes = "0" + timeDate.getMinutes();
+        var hours = "0" + timeDate.getHours();
+        var date = "0" + timeDate.getDate();
+        var month = "0" + (timeDate.getMonth() + 1);
+        var monthRus = this.rusMonths[+month];
+        var year = timeDate.getFullYear();
+
+        if (true === format) {
+            return hours.substr(-2) + ':' + minutes.substr(-2);
+        } else if (false === format) {
+            return date.substr(-2) + ' ' + monthRus + ' ' + year;
+        }
     }
 };
