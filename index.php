4825bd2fbd5dc87120d2f7b6c4be3a37a67141ba
Witalij Kaa <witalijkaa@hotmail.com>
2016-04-28 18:19:42 +0300

    окончание javaScript

--- index.php
+++ index.php
@@ -7,30 +7,5 @@
 <body>
 <h1>экспериментальная страница</h1>
 
-
-<h3>аналог ООП в javaScript</h3>
-<script src="./javaScript/jsOOP.js"></script>
-
-<h3>свойства Object</h3>
-<script type="text/javascript" src="./javaScript/propsObject.js"></script>
-
-<h3>работа с document</h3>
-<script src="./javaScript/workDOM.js"></script>
-<span></span><p id="pEl">startText</p>
-<div id="divRadio">
-Да<input type="radio" name="answer" value="Да" />
-Нет<input type="radio" name="answer" value="Нет" />
-Не знаю<input type="radio" name="answer" value="Не знаю" />
-</div>
-<pre id="preEl"></pre>
-<div id="divForCreate"></div>
-
-<h3>обработка событий</h3>
-<script src="./javaScript/events.js"></script>
-<div id="strContainer">
-    <input type="text">
-    <input type="text">
-</div>
-
 </body>
 </html>
