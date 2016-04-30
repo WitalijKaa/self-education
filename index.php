95a30ff2303ec922456549ec7bf4a2a8a4b20ed1
Witalij Kaa <witalijkaa@hotmail.com>
2016-04-30 15:10:55 +0300

    некоторые jQuery

--- index.php
+++ index.php
@@ -3,9 +3,127 @@
 <head>
     <meta charset="UTF-8">
     <title>THML title</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.js"></script>
+    <script src="/javaScript/jQueryColorPlugin.js"></script>
+    <script src="/javaScript/jQueryColorPluginExt.js"></script>
 </head>
 <body>
 <h1>экспериментальная страница</h1>
+
+<div>
+<span>
+<a href="http://edu.cbsystematics.com/video/ru">Видео обучение</a><br />
+<a href="http://jquery.com">jQuery</a><br />
+<a href="http://htmlbook.ru/">HTML-book</a>
+</span>
+</div>
+
+<div><p>мало букафф</p></div>
+<div><p>расс</p><p>дффа</p></div>
+<div><p>трры</p><p>ччри</p></div>
+
+<input type="checkbox" checked="checked">
+
+<ul class="the-count">
+    <li>кудатам</li>
+    <li>кудатам</li>
+    <li>кудатам</li>
+    <li>кудатам</li>
+    <li id="five">кудатам 5</li>
+    <li>кудатам</li>
+    <li>кудатам</li>
+    <li>кудатам</li>
+    <li>кудатам</li>
+    <li>кудатам 10</li>
+    <li data-desc="new can jojo">кудатам</li>
+    <li data-desc="new-car jojo">кудатам</li>
+    <li data-desc="new cat">кудатам</li>
+    <li>кудатам</li>
+    <li>кудатам 15</li>
+    <li>кудатам</li>
+    <li>кудатам</li>
+    <li>кудатам</li>
+    <li>кудатам</li>
+    <li>кудатам 20</li>
+    <li>кудатам</li>
+    <li>кудатам</li>
+    <li>кудатам</li>
+    <li>кудатам</li>
+    <li>кудатам 25</li>
+    <li>кудатам</li>
+    <li>кудатам</li>
+    <li>кудатам</li>
+    <li>кудатам</li>
+</ul>
+
+<div id="animka" style="position: relative; width: 300px; height: 250px; border: 1px solid darkcyan; color: aliceblue"></div>
+
+<script>
+
+$(function () {
+
+    $("a[href^='http://']").css("color", "red");    // Выбор всех элементов "a", с атрибутом "href", который начинается c "http://"
+    $("a[href$='.com']").css("color", "blue");      // Выбор всех элементов "a", с атрибутом "href", который заканчивается на ".com"
+    $("a[href*='html']").css("color", "green");     // Выбор всех элементов "a", с атрибутом "href", который содержит "html"
+
+    $("div:has(a)").css("border", "2px solid darkMagenta");
+    $("div p:only-child").css("color", "darkMagenta");
+    //$("li:even").css("background-color", "red");
+    //$("li:odd").css("background-color", "red");
+    $(".the-count li:nth-child(3n+1)").css("background-color", "lightblue");
+    $("p:not(p:eq(3))").css("background-color", "lightgray");
+
+    $("li[data-desc~='new']").css("background-color", "lightgray");
+
+    setTimeout(theAnim, 12000);
+
+    function theAnim() {
+        $(".the-count").hide(3000);
+        $(":animated").css("font-size", "20px");
+    }
+
+    $('#five').attr({kube: '', 'data-nice': 'pretty'});
+
+    $("input").change(function () {
+        var $input = $(this);
+        $("p:eq(1)").html(
+            ".attr( \"checked\" ): <b>" + $input.attr("checked") + "</b><br>" +
+            ".prop( \"checked\" ): <b>" + $input.prop("checked") + "</b><br>") + "</b>";
+    });
+
+//    $("input").prop('checked', false);
+//    $("input").removeAttr('checked');
+
+
+//    $("div").on("click", function () {
+//        if ($(this).is(":first-child")) {
+
+    $("#animka").click(function () {
+        $(this)
+            .animate({ left: "200px" }, 800)
+            .animate({ width: "800px" }, 600)
+            .animate({'background-color': 'red'}, 600)
+            .queue(function () {
+                $(this).animate({'background-color': '#ff4500'}, 600);
+                var divThis = this;
+                setTimeout(function () {
+                    $(divThis).css('background', "#ff4500");
+                }, 600);
+                setTimeout(function () {
+                    $(divThis).css('background', "green");
+                }, 1200);
+                setTimeout(function () {
+                    $(divThis).css('background', "darkblue");
+                }, 1800);
+                setTimeout(function () {
+                    $(divThis).dequeue();
+                }, 2200);
+            })
+            .animate({ left: "10px", top: "30px" }, 800);
+    });
+});
+
+</script>
 
 </body>
 </html>
