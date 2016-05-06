bd8b6ae81db77716990ff22ec2d10bd35141aac5
Witalij Kaa <witalijkaa@hotmail.com>
2016-05-06 17:27:37 +0300

    короткий AngularJS

--- index.php
+++ index.php
@@ -1,13 +1,38 @@
 <!DOCTYPE html>
-<html>
+<html ng-app="educationApp">
 <head>
     <meta charset="UTF-8">
     <title>THML title</title>
-    <script src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-2.2.3.js" type="javascript"></script>
-    <script src="/javaScript/wkphp.js" type="javascript"></script>
+    <script src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-2.2.3.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular.js"></script>
+    <script src="/javaScript/wkphp.js"></script>
+    <script>
+        var model = {
+            some: "статические данные",
+            c: 1
+        };
+        angular.module("educationApp", []).controller("basicCtrl", function ($scope, $interval) {
+            $scope.model = model;
+
+            $scope.clickHandler = function () { $scope.model.some += ' and'; };
+
+            $interval(function () { $scope.model.c++; }, 1000);
+//            setInterval(function () { $scope.model.c++; $scope.$digest(); }, 1000);
+//            setInterval(function () { $scope.$apply(function () { $scope.model.c++; }); }, 1000);
+        });
+    </script>
 </head>
-<body>
-<h1>экспериментальная страница</h1>
+<body ng-controller="basicCtrl">
+
+<h1>изучение AngularJS</h1>
+<h2>{{model.some}}</h2>
+<input ng-model="model.some" />
+<button ng-click="clickHandler()">Write</button>
+
+<br>
+<br>
+<br>
+<pre>{{model.c}}</pre>
 
 </body>
 </html>
